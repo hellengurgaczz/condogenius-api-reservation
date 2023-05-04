386c760888c5d09bd39df0cf59bfcf8572985ec3
hellen <gurgaczzhellen@gmail.com>
2023-05-04 19:37:51 -0300

    adicionando doc swagger

--- Program.cs
+++ Program.cs
@@ -31,15 +31,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 
-    app.UseSwagger();
-
-    app.UseSwaggerUI(c =>
-    {
-        c.RoutePrefix = "swagger";
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API V1");
-    });
-
 }
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API v1");
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
