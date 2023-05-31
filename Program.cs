597d4bd6729d6e964c083059d9cfb4483d918984
hellen gurgacz <hellen.gurgacz@g.globo>
2023-05-31 18:12:47 -0300

    consumer rabbitmq

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 using condogenius_api_reservation.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using Newtonsoft.Json;
+using condogenius_api_reservation.Controllers;
+using condogenius_api_reservation.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +17,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>
 (
-    options => options.UseMySQL("server=localhost;port=3306;database=condogenius;user=root;password=261315")
+    options => options.UseMySQL("server=localhost;port=3306;database=condogenius;user=root")
 );
 
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddCors(options => {
     options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.AddSingleton<IModel>(serviceProvider =>
+{
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+    var connection = factory.CreateConnection();
+    var channel = connection.CreateModel();
+    return channel;
 });
 
 builder.Services.AddSwaggerGen(c =>
@@ -49,5 +66,37 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Consumer
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    var context = serviceProvider.GetRequiredService<DataContext>();
+    var channel = serviceProvider.GetRequiredService<IModel>();
+
+    string queueName = "reservations";
+    channel.QueueDeclare(queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("Mensagem recebida: {0}", message);
+
+        // Converter a mensagem para o objeto Reservation
+        var reservation = JsonConvert.DeserializeObject<Reservation>(message);
+
+        // Chamar o método Create do ReservationsController para criar a reserva
+        var reservationsController = new ReservationsController(context);
+        Console.WriteLine(reservationsController.Create(reservation));
+
+    };
+
+    channel.BasicConsume(queueName, autoAck: true, consumer: consumer);
+
+    Console.ReadLine();
+}
+
 
 app.Run();
